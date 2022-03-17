6ecf099101ebf22db2eaa55426f3264c6f2df2da
Christy Seddon <89155206+CSeddon01@users.noreply.github.com>
2022-03-17 14:34:28 -0500

    Defined columns to models

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -5,12 +5,28 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'user_id'
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'user_id'
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  // through: Vote,
+  // as: 'voted_posts',
+  foreignKey: 'user_id'
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  // through: Vote,
+  // as: 'voted_posts',
+  foreignKey: 'user_id'
+});
 
 module.exports = {
   Product,
